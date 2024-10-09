60835c33cdbf1b39c94bdfc69298acaaab8fa2d8
Jeff Huang <huangjeff@google.com>
2024-10-09 11:48:48 -0500

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,27 @@
 import { configureGenkit } from '@genkit-ai/core';
 import { dotprompt } from '@genkit-ai/dotprompt';
 import { startFlowsServer } from '@genkit-ai/flow';
-import { customerServiceFlow } from './customerServiceFlow';
 import { openAI } from 'genkitx-openai';
-import { googleAI } from '@genkit-ai/googleai';
-import { vertexAI } from '@genkit-ai/vertexai';
+import { gemini15Flash, googleAI } from '@genkit-ai/googleai';
+import { textEmbeddingGecko, vertexAI } from '@genkit-ai/vertexai';
 
 // Configure Genkit with necessary plugins
 configureGenkit({
   plugins: [
     vertexAI({ projectId: 'cloud-llm-preview1', location: 'us-central1' }),
-    dotprompt(), // Load the Dotprompt plugin
+    dotprompt(), // Load the Dotprompt plugin,
+    genkitEval({
+      judge: gemini15Flash,
+      metrics: [GenkitMetric.FAITHFULNESS, GenkitMetric.ANSWER_RELEVANCY],
+      embedder: textEmbeddingGecko, // GenkitMetric.ANSWER_RELEVANCY requires an embedder
+    }),
   ],
   logLevel: 'debug', // Set log level to debug for development
   enableTracingAndMetrics: true, // Enable tracing and metrics
 });
+
+import { customerServiceFlow } from './customerServiceFlow';
+import genkitEval, { GenkitMetric } from '@genkit-ai/evaluator';
 
 // Export the flow(s) you've defined
 export { customerServiceFlow };
