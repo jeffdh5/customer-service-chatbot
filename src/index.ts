51e59503221180eb8d17589b706ae75502612ab4
Jeff Huang <huangjeff@google.com>
2024-10-09 17:28:15 -0500

    add generateDraft evals

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,17 @@ import { startFlowsServer } from '@genkit-ai/flow';
 import { openAI } from 'genkitx-openai';
 import { gemini15Flash, googleAI } from '@genkit-ai/googleai';
 import { textEmbeddingGecko, vertexAI } from '@genkit-ai/vertexai';
+import genkitEval, { GenkitMetric } from '@genkit-ai/evaluator';
 
 // Configure Genkit with necessary plugins
 configureGenkit({
   plugins: [
     vertexAI({ projectId: 'cloud-llm-preview1', location: 'us-central1' }),
+    googleAI(),
     dotprompt(), // Load the Dotprompt plugin,
     genkitEval({
       judge: gemini15Flash,
-      metrics: [GenkitMetric.FAITHFULNESS, GenkitMetric.ANSWER_RELEVANCY],
+      metrics: [GenkitMetric.FAITHFULNESS],
       embedder: textEmbeddingGecko, // GenkitMetric.ANSWER_RELEVANCY requires an embedder
     }),
   ],
@@ -21,7 +23,6 @@ configureGenkit({
 });
 
 import { customerServiceFlow } from './customerServiceFlow';
-import genkitEval, { GenkitMetric } from '@genkit-ai/evaluator';
 
 // Export the flow(s) you've defined
 export { customerServiceFlow };
