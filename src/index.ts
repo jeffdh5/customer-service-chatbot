b0cc89d6b8d9a322f9948d8a18e247c7d8ec271f
Jeff Huang <huangjeff@google.com>
2024-10-10 11:39:57 -0500

    add logging and telemetry

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
 import { configureGenkit } from '@genkit-ai/core';
 import { dotprompt } from '@genkit-ai/dotprompt';
 import { startFlowsServer } from '@genkit-ai/flow';
-import { openAI } from 'genkitx-openai';
 import { gemini15Flash, googleAI } from '@genkit-ai/googleai';
 import { textEmbeddingGecko, vertexAI } from '@genkit-ai/vertexai';
 import genkitEval, { GenkitMetric } from '@genkit-ai/evaluator';
+import { googleCloud } from '@genkit-ai/google-cloud';
+import { AlwaysOnSampler } from '@opentelemetry/sdk-trace-base';
 
 // Configure Genkit with necessary plugins
 configureGenkit({
   plugins: [
-    vertexAI({ projectId: 'cloud-llm-preview1', location: 'us-central1' }),
+    vertexAI({ projectId: 'genkit-devrel-samples', location: 'us-central1' }),
     googleAI(),
     dotprompt(), // Load the Dotprompt plugin,
     genkitEval({
@@ -17,9 +18,27 @@ configureGenkit({
       metrics: [GenkitMetric.FAITHFULNESS],
       embedder: textEmbeddingGecko, // GenkitMetric.ANSWER_RELEVANCY requires an embedder
     }),
+    googleCloud({
+      projectId: 'genkit-devrel-samples',
+      telemetryConfig: {
+        forceDevExport: true, // Set this to true to export telemetry for local runs
+        sampler: new AlwaysOnSampler(),
+        autoInstrumentation: true,
+        autoInstrumentationConfig: {
+          '@opentelemetry/instrumentation-fs': { enabled: false },
+          '@opentelemetry/instrumentation-dns': { enabled: false },
+          '@opentelemetry/instrumentation-net': { enabled: false },
+        },
+        metricExportIntervalMillis: 5_000,
+      },
+    })
   ],
   logLevel: 'debug', // Set log level to debug for development
   enableTracingAndMetrics: true, // Enable tracing and metrics
+  telemetry: {
+    instrumentation: 'googleCloud',
+    logger: 'googleCloud',
+  },
 });
 
 import { customerServiceFlow } from './customerServiceFlow';
